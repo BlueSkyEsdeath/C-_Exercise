f25d9a49e54f4f06a339728d04190b01e5e5f224
Tuan Hoang <tuanhoang121199@gmail.com>
2022-12-05 22:52:33 +0700

    bubbleSort Exercise

---
+++ AlgorithmExercise/AlgorithmExercise/FamiliarWithString.cs
@@ -0,0 +1,6 @@
+// string name = "Tuan";
+// Console.WriteLine("Hello," + name + " : " + name.Length);
+// Console.WriteLine(string.Format("Hello, {0} : {1} - {2}" , name, name.Length, "sa"));
+// Console.WriteLine($"Hello, {name} : {name.Length} - {name}");
+// name = name + $" : {name.Length}";
+// Console.WriteLine(name);
